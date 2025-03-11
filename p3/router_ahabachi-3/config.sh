e60ead2438786142a2a00fb7638500099e14743b
Ousama Beaj <obeaj@e2r8p15.1337.ma>
2025-03-11 12:40:22 +0000

    p3-obeaj

--- p3/router_ahabachi-3/config.sh
+++ p3/router_ahabachi-3/config.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+
+# ip link add br0 type bridge
+# ip link set dev br0 up
+# ip link add vxlan10 type vxlan id 10 dstport 4789 local 1.1.1.3 nolearning
+# ip link set dev vxlan10 up
+# brctl addif br0 vxlan10
+# brctl addif br0 eth0
+
 vtysh
 configure terminal
 
 no ipv6 forwarding
 !
-interface eth0
+interface eth1
     ip address 10.1.1.6/30
     ip ospf area 0
 !
