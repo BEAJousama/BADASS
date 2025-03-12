d5ce4593780184d961184d50a6c25d069cfd60db
Ousama Beaj <obeaj@e2r11p2.1337.ma>
2025-03-12 16:58:21 +0000

    add comments to P2

--- P2/router_ahabachi-1/dynamic_multicast.sh
+++ P2/router_ahabachi-1/dynamic_multicast.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 
-# ...
+# Create a Linux bridge interface
+# This will connect regular interfaces with the VXLAN overlay
 ip link add br0 type bridge
 
-# ...
+# Activate the bridge interface
 ip link set dev br0 up
 
-# ...
+# Assign IP address to physical interface eth0
+# This IP will be used for underlay network communication
 ip addr add 10.1.1.1/24 dev eth0
 
-# create vxlan interface
+# Create VXLAN interface using multicast for BUM traffic
+# - id 10: VXLAN Network Identifier (VNI) is 10
+# - dev eth0: Physical interface used for VXLAN traffic
+# - group 239.1.1.1: Multicast group address for broadcast/unknown/multicast traffic
+# - dstport 4789: Standard VXLAN UDP port
 ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
 
-# ...
+# Assign IP address to VXLAN interface
+# This creates a routed VXLAN interface (L3VXLAN) 
+# Hosts in the 20.1.1.0/24 subnet can use this as a gateway
 ip addr add 20.1.1.1/24 dev vxlan10
 
-# ...
+# Add physical interface eth1 to the bridge
+# Local devices connected to eth1 will be part of the bridged domain
 brctl addif br0 eth1
 
-# ...
+# Add VXLAN interface to the bridge
+# This bridges the VXLAN tunnel with the local interface
+# NOTE: This creates a mixed L2/L3 configuration that may cause unpredictable behavior
+# Typically, you either assign an IP to vxlan10 OR bridge it, not both
 brctl addif br0 vxlan10
 
-# ...
+# Activate the VXLAN interface
 ip link set dev vxlan10 up
